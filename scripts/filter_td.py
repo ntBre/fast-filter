f0849f9d901b17adb1723e2bd3496a2cc0bfcdd1
Brent Westbrook <brentrwestbrook@gmail.com>
2023-07-26 18:17:54 -0400

    confirm that I can round trip without messing anything up

---
+++ scripts/filter_td.py
@@ -0,0 +1,52 @@
+import json
+
+from openff.qcsubmit.results.filters import (
+    ConnectivityFilter,
+    RecordStatusFilter,
+    UnperceivableStereoFilter,
+    HydrogenBondFilter,
+    ElementFilter,
+)
+
+from openff.qcsubmit.results import TorsionDriveResultCollection
+from qcportal.models.records import RecordStatusEnum
+from openff.qcsubmit.results.filters import ResultRecordFilter
+
+
+class ChargeCheckFilter(ResultRecordFilter):
+    def _filter_function(self, result, record, molecule) -> bool:
+        from openff.toolkit.utils.exceptions import (
+            UnassignedMoleculeChargeException,
+        )
+
+        # Some of the molecules fail charging with am1bccelf10 either
+        # because of no bccs or failed conformer generation, sometimes it
+        # cannot be captured with just the cmiles present in the record
+        # metadata, so reading from file and checking it
+        can_be_charged = True
+        try:
+            molecule.assign_partial_charges(
+                partial_charge_method="am1bccelf10"
+            )
+        except (UnassignedMoleculeChargeException, ValueError):
+            can_be_charged = False
+
+        return can_be_charged
+
+
+entries = dict(json.loads(r"""{json}"""))
+dataset = TorsionDriveResultCollection(entries=entries)
+
+# TODO pass in include_iodine
+elements = ["H", "C", "N", "O", "S", "P", "F", "Cl", "Br"]
+
+# dataset = dataset.filter(
+#     HydrogenBondFilter(method="baker-hubbard"),
+#     RecordStatusFilter(status=RecordStatusEnum.complete),
+#     ConnectivityFilter(tolerance=1.2),
+#     UnperceivableStereoFilter(),
+#     ElementFilter(allowed_elements=elements),
+#     ChargeCheckFilter(),
+# )
+
+print(dataset.json(indent=2))
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::{collections::HashMap, process::Command};
+
+use openff_toolkit::qcsubmit::results::TorsionDriveResultCollection;
+
+use rayon::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let ds = TorsionDriveResultCollection::parse_file(
+        "/home/brent/omsf/projects/valence-fitting/02_curate-data/datasets/\
+	 filtered-sage-td.json",
+    )
+    .unwrap();
+
+    let mut results = Vec::new();
+    let ds_name = ds.entries.iter().next().unwrap().0;
+    for (name, entries) in &ds.entries {
+        results = entries
+            .par_iter()
+            .enumerate()
+            .map(|(i, entry)| {
+                let map = HashMap::from([(name.to_owned(), [entry])]);
+                let json = serde_json::to_string(&map).unwrap();
+                let script = include_str!("../scripts/filter_td.py")
+                    .replace("{json}", &json);
+                let mut cmd = Command::new("python");
+                let output = cmd.arg("-c").arg(&script).output().unwrap();
+
+                if !output.status.success() {
+                    println!("{script}");
+                    panic!("{}", String::from_utf8_lossy(&output.stderr));
+                }
+
+                let ds: TorsionDriveResultCollection =
+                    serde_json::from_slice(&output.stdout).unwrap();
+                eprintln!("finished {i}");
+                ds.entries.into_values().flatten().collect::<Vec<_>>()
+            })
+            .flatten()
+            .collect();
+    }
+
+    let got = TorsionDriveResultCollection {
+        entries: HashMap::from([(ds_name.clone(), results)]),
+    };
+
+    assert_eq!(got, ds);
 }
